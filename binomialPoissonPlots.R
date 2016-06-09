5847ad330ec3e5727448973299b7ba342fa5f0d0
GrizAdams <GrizAdams@users.noreply.github.com>
2016-06-08 21:45:44 -0400

    Delete binomialPoissonPlots.R

--- binomialPoissonPlots.R
+++
@@ -1,8 +0,0 @@
-mtcars31 <- mtcars[-9, ]
-
-fitlog <- glm(am ~ qsec, family='binomial', mtcars31)
-fitpos <- glm(mpg ~ am + qsec, family='poisson', mtcars31)
-
-g <- ggplot(mtcars31, aes(x = qsec, y = fitpos$residuals, color = am)) + 
-  geom_point() + geom_line(y=0) + geom_smooth(method=poisson)
-g
