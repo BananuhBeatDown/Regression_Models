259f28a664ee6720e7cd3ccacaed6aae1332b134
Matt Green <GrizAdams@users.noreply.github.com>
2016-07-11 16:35:06 -0400

    Delete quiz3qu4.R

--- quizes/quiz3qu4.R
+++
@@ -1,6 +0,0 @@
-fit <- lm(mpg ~ I(wt * 0.5) + factor(cyl) + wt, mtcars)
-fit
-summary(fit)
-# The estimated expected change in mpg per one ton
-# increase in weight for a specific
-# number of cylinders (4, 6, 8)
