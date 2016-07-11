887c7d9fe43d8a430302a7209c21b3be7c368aa1
Matt Green <GrizAdams@users.noreply.github.com>
2016-07-11 16:34:42 -0400

    Delete quiz3qu6.R

--- quizes/quiz3qu6.R
+++
@@ -1,8 +0,0 @@
-# Give the hat diagonal for the most influential point
-# use dfbeatas(), NOT dfbeta()
-
-x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
-y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
-fit <- lm(y ~ x)
-dfbetas(fit)
-cbind(dfbetas(fit), dfbeta(fit))
