9089d7a747026f89dc8f8e14edc246013fe8feda
Matt Green <GrizAdams@users.noreply.github.com>
2016-07-11 16:32:50 -0400

    Delete quiz3qu3.R

--- quizes/quiz3qu3.R
+++
@@ -1,5 +0,0 @@
-fit <- lm(mpg ~ factor(cyl) + wt, mtcars)
-fit2 <- lm(mpg ~ factor(cyl) * wt, mtcars)
-anova(fit, fit2, test="Chisq")
-# since we have a large P-value, that is a good indicator
-# to fail to reject and the interaction term is not necessary
