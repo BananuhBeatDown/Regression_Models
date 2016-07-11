8e0f44f7b81f48e2a2a2ddfdc44bd15a9cd7e70f
Matt Green <GrizAdams@users.noreply.github.com>
2016-07-11 16:31:49 -0400

    Delete quiz2qu3.R

--- quizes/quiz2qu3.R
+++
@@ -1,7 +0,0 @@
-# need to use "interval=confidence" in the predict function
-
-fit <- lm(mpg ~ wt, mtcars)
-
-meanwt = data.frame(wt = mean(mtcars$wt))
-
-predict(fit, meanwt, interval="confidence")
