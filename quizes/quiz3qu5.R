90c99d62816f85c86f30acbc4d350f65cbb62e95
Matt Green <GrizAdams@users.noreply.github.com>
2016-07-11 16:34:16 -0400

    Delete quiz3qu5.R

--- quizes/quiz3qu5.R
+++
@@ -1,7 +0,0 @@
-# Give the hat diagonal for the most influential point
-
-x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
-y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
-
-fit <- lm(y ~ x)
-hatvalues(fit)
