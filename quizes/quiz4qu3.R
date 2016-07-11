607988dbfb28e7c7e216c2c782bec522463236af
Matt Green <GrizAdams@users.noreply.github.com>
2016-07-11 16:36:42 -0400

    Delete quiz4qu3.R

--- quizes/quiz4qu3.R
+++
@@ -1,6 +0,0 @@
-#create new binary column in shuttle data representing the 'use' column
-shuttle$usebin <- as.numeric(shuttle$use == 'auto')
-fit1 <- glm(usebin ~ wind - 1, family=binomial(link = 'logit'), shuttle)
-fit2 <- glm(I(1- usebin) ~  wind - 1, family='binomial', shuttle)
-summary(fit1)
-summary(fit2)
